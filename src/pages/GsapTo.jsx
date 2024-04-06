56e52ccb4f0d26cd9cd376be91f9da6681e5c523
PersianDavinci <persiandavinci@gmail.com>
2024-04-06 14:48:31 +0330

    add some mental methods to GsapTo.jsx to test

--- src/pages/GsapTo.jsx
+++ src/pages/GsapTo.jsx
@@ -1,5 +1,19 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
 const GsapTo = () => {
   // TODO: Implement the gsap.to() method
+
+  useGSAP(() => {
+    gsap.to("#blue-box", {
+      x: 250,
+      repeat: -1,
+      yoyo: true,
+      rotation: 360,
+      duration: 2,
+      ease: "elastic",
+    });
+  }, []);
 
   return (
     <main>
