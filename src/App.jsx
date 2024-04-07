f5384043dfa8ab5a2285165106c19f6e20fea3c6
PersianDavinci <persiandavinci@gmail.com>
2024-04-07 14:26:01 +0330

    add 3 components named as navbar hero highlights with base rafce

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return <main className="bg-black">Navbar Hero Highlights</main>;
 };
 
 export default App;
