bba843c69ed7b99caff68b24cf8f7c669db450b2
PersianDavinci <persiandavinci@gmail.com>
2024-04-07 15:21:38 +0330

    npm install gsap @gsap/react and start for hero components

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,11 @@
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section className="w-full nav-hight bg-black relative">
+      <div className="h-5/6 w-full flex-col flex-center">
+        <p className="hero-title">iPhone 15 Pro</p>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
