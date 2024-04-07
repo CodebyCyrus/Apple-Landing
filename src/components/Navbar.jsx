ef6b13bb6763328fc5d392e91c2913c15e061492
PersianDavinci <persiandavinci@gmail.com>
2024-04-07 14:58:45 +0330

    add navbar some detail

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,23 @@
+import { appleImg, bagImg, searchImg } from "../utils";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <header>
+      <nav>
+        <img src={appleImg} alt="Apple Img" width={14} height={18} />
+
+        <div>
+          {["iPhone", "Macbook", "iPad"].map((nav) => (
+            <div key={nav}>{nav}</div>
+          ))}
+        </div>
+        <div>
+          <img src={searchImg} alt="Search icon" width={18} height={18} />
+          <img src={bagImg} alt="Buy icon" width={18} height={18} />
+        </div>
+      </nav>
+    </header>
+  );
 };
 
 export default Navbar;
