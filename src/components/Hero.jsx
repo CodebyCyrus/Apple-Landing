b554c631f5042e64861d2576e4121af0e32e2671
PersianDavinci <persiandavinci@gmail.com>
2024-04-07 15:25:06 +0330

    add first gsap animate to hero components

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,17 @@
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+
 const Hero = () => {
+  useGSAP(() => {
+    gsap.to("#hero", { opacity: 1, delay: 1.5 });
+  }, []);
+
   return (
     <section className="w-full nav-hight bg-black relative">
       <div className="h-5/6 w-full flex-col flex-center">
-        <p className="hero-title">iPhone 15 Pro</p>
+        <p id="hero" className="hero-title">
+          iPhone 15 Pro
+        </p>
       </div>
     </section>
   );
