3e9dc9d94b529ce8104c1dd3d3e8a66d3b62d388
PersianDavinci <persiandavinci@gmail.com>
2024-04-07 15:14:00 +0330

    finish navbar part A

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,23 @@
 import { appleImg, bagImg, searchImg } from "../utils";
 
+import { navLists } from "../constants";
 const Navbar = () => {
   return (
-    <header className="w-full">
-      <nav>
+    <header className="w-full py-5 sm:px-10 px-5 flex justify-between items-center">
+      <nav className="flex w-full screen-max-width">
         <img src={appleImg} alt="Apple Img" width={14} height={18} />
 
-        <div>
-          {["iPhone", "Macbook", "iPad"].map((nav) => (
-            <div key={nav}>{nav}</div>
+        <div className="flex flex-1 justify-center max-sm:hidden">
+          {navLists.map((nav) => (
+            <div
+              key={nav}
+              className="px-5 text-sm cursor-pointer text-gray hover:text-white translate-all"
+            >
+              {nav}
+            </div>
           ))}
         </div>
-        <div>
+        <div className="flex items-baseline gap-7 max-sm:justify-end max-sm:flex-1">
           <img src={searchImg} alt="Search icon" width={18} height={18} />
           <img src={bagImg} alt="Buy icon" width={18} height={18} />
         </div>
