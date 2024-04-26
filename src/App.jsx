1bf8d8a1826d0252798a96b714e07731f8e47c7f
PersianDavinci <persiandavinci@gmail.com>
2024-04-26 12:24:08 +0330

    import

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Highlights from "./components/Highlights";
+import Model from "./components/Model";
 
 const App = () => {
   return (
